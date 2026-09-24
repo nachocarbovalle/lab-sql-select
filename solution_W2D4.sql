576a3668ac9ba4811f198154e0a268486964fe9f
nachocarbovalle <nachocv98@gmail.com>
2026-09-24 13:37:37 +0200

    Solution lab W2D4

---
+++ solution_W2D4.sql
@@ -0,0 +1,50 @@
+-- Challenge 1: Who has published what, and where?
+SELECT
+    a.au_id    AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    t.title    AS "TITLE",
+    p.pub_name AS "PUBLISHER"
+FROM authors a
+JOIN titleauthor ta ON a.au_id    = ta.au_id
+JOIN titles t       ON ta.title_id = t.title_id
+JOIN publishers p   ON t.pub_id    = p.pub_id;
+
+-- Challenge 2: Who Have Published How Many At Where?
+SELECT
+    a.au_id    AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    p.pub_name AS "PUBLISHER",
+    COUNT(t.title_id) AS "TITLE COUNT"
+FROM authors a
+JOIN titleauthor ta ON a.au_id    = ta.au_id
+JOIN titles t       ON ta.title_id = t.title_id
+JOIN publishers p   ON t.pub_id    = p.pub_id
+GROUP BY a.au_id, a.au_lname, a.au_fname, p.pub_name
+ORDER BY "TITLE COUNT" DESC;
+
+-- Challenge 3: Best Selling Authors
+SELECT
+    a.au_id    AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    SUM(s.qty) AS "TOTAL"
+FROM authors a
+JOIN titleauthor ta ON a.au_id     = ta.au_id
+JOIN sales s        ON ta.title_id = s.title_id
+GROUP BY a.au_id, a.au_lname, a.au_fname
+ORDER BY "TOTAL" DESC
+LIMIT 3;
+
+-- Challenge 4: The ranking of all 23 authors, including those with 0 sales
+SELECT
+    a.au_id    AS "AUTHOR ID",
+    a.au_lname AS "LAST NAME",
+    a.au_fname AS "FIRST NAME",
+    COALESCE(SUM(s.qty), 0) AS "TOTAL"
+FROM authors a
+LEFT JOIN titleauthor ta ON a.au_id     = ta.au_id
+LEFT JOIN sales s        ON ta.title_id = s.title_id
+GROUP BY a.au_id, a.au_lname, a.au_fname
+ORDER BY "TOTAL" DESC;
